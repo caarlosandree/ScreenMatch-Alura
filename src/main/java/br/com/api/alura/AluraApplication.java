040250ca0c7e0e333d52cbc781e96ac420c2fa88
caarlosandree <caarlosandree@gmail.com>
2026-05-05 09:13:07 -0300

    Add project structure and configuration files

---
+++ src/main/java/br/com/api/alura/AluraApplication.java
@@ -0,0 +1,25 @@
+package br.com.api.alura;
+
+import br.com.api.alura.service.ConsumoAPI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraApplication implements CommandLineRunner {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    static void main(String[] args) {
+        SpringApplication.run(AluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoAPI = new ConsumoAPI();
+        var json = consumoAPI.obterDados("http://www.omdbapi.com/?apikey=" + apiKey + "&t=Breaking+Bad&Season=1");
+        System.out.println(json);
+    }
+}
